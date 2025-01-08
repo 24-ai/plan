7b1d1c4385d72766e533fef7dc9c9cbe6719bce1
24-ai <jonathan.wetherley@gmail.com>
2025-01-08 19:52:40 +0000

    Add simple test component

---
+++ src/app/components/MondayMeals.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+
+const MondayMeals = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-bold">Monday&apos;s Meal Plan</h2>
+      <p>Test Component</p>
+    </div>
+  );
+};
+
+export default MondayMeals;
